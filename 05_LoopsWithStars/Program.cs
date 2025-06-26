3c5b5ebafb2f3aa97226920ac560f4d646fef568
İrem Koşar <99809817+iremkosar@users.noreply.github.com>
2025-06-26 17:10:46 +0300

    LoopsWithStars

---
+++ 05_LoopsWithStars/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_LoopsWithStars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Alt Alta 10 tane yıldız oluşturma
+            //for(int i=1; i<=10; i++)
+            //{
+            //    Console.WriteLine("*");
+            //}
+            #endregion
+
+            #region Yan yana 10 tane yıldız oluşturma
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.Write("*");
+            //}
+            #endregion
+
+            #region Alt Alta 10 tane yıldız oluşturma her satırda 10 tane yıldız olsun
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.WriteLine("**********");
+            //}
+            #endregion
+
+            #region Dik üçgen Çizme
+            //for (int i = 1; i <= 5; i++)
+            //{
+            //       for(int j=1; j<=i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine(); 
+            //}
+            #endregion
+
+            #region Ters Dik üçgen Çizme
+            //for(int i=5; i>=1; i--)
+            //{
+            //    for(int j=1; j<=i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+            #endregion
+
+            #region Dik ve Ters üçgen Beraber    
+            //for(int i=1; i<=5; i++)
+            //{
+            //    for(int j=1; j<=i; j++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+            //for(int k=4; k>=1; k-- )
+            //{
+            //    for(int m=1; m<=k; m++)
+            //    {
+            //        Console.Write("*");
+            //    }
+            //    Console.WriteLine();
+            //}
+            #endregion
+
+            Console.Read();
+        }
+    }
+}
